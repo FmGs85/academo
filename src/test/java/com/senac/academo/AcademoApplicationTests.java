d65d2d8b27a0ca5175b2af7f7008b767fb26ae62
fabio <fabiomelogs85@gmail.com>
2025-10-15 11:54:49 -0300

    Primeiro commit

---
+++ src/test/java/com/senac/academo/AcademoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.academo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
